65a16ad37aa0bcea4bf2ce8d24b6ce5635d23508
Jimmy Jonson <ap412@bk.ru>
2025-04-07 22:46:28 +0300

    Fixing test bugs

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -6,8 +6,8 @@ author varchar(255) NOT NULL,
 isbn varchar(255) UNIQUE NOT NULL,
 price float8 NOT NULL,
 title varchar(255) NOT NULL,
-publisher varchar(255) NULL,
 created_date timestamp NOT NULL,
 last_modified_date timestamp NOT NULL,
-version integer NOT NULL
+version integer NOT NULL,
+publisher varchar(255) NULL
 );
